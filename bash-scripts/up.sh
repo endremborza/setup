199dab9ca5e969d3c855e9a07f83be4118b5f2d8
Endre Márk Borza <endremborza@gmail.com>
2024-04-08 01:39:39 +0200

    update setup

--- bash-scripts/up.sh
+++ bash-scripts/up.sh
@@ -1,7 +1,21 @@
 sudo apt update
-sudo apt install file git make cmake ninja-build gettext unzip curl build-essential -y
+sudo apt install file git make cmake ninja-build gettext unzip curl build-essential stow python 3.10 -y
 
 
 git clone --branch v0.9.5 --depth 1 https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+
+cd ~
+
+git clone https://github.com/endremborza/setup
+cd setup
+stow -n --verbose=3 -t ~ dotfiles
+
+
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install node
