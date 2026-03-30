58627146eb30e03aaf4d2d20ec60ea1c101adac9
Endre Márk Borza <endremborza@gmail.com>
2026-03-30 15:05:32 +0200

    rm snippets, add paths

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -156,23 +156,12 @@ require('lazy').setup({
     'hrsh7th/nvim-cmp',
     event = "BufReadPre",
     dependencies = {
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-nvim-lsp',
-      'rafamadriz/friendly-snippets',
+      'hrsh7th/cmp-path',
     },
     config = function()
       local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      require('luasnip.loaders.from_vscode').lazy_load()
-      luasnip.config.setup {}
-
       cmp.setup {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
         completion = {
           completeopt = 'menu,menuone,noinsert',
         },
@@ -187,27 +176,15 @@ require('lazy').setup({
             select = true,
           },
           ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
+            if cmp.visible() then cmp.select_next_item() else fallback() end
           end, { 'i', 's' }),
           ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
+            if cmp.visible() then cmp.select_prev_item() else fallback() end
           end, { 'i', 's' }),
         },
         sources = {
           { name = 'nvim_lsp' },
-          { name = 'luasnip' },
+          { name = 'path',    option = { get_cwd = function() return vim.fn.getcwd() end } },
         },
       }
     end,
